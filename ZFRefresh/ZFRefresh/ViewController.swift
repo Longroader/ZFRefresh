58f264f0745460fb2c28cc47248ee38aabaa7baf
开心做人 <234283063@qq.com>
2017-02-08 20:09:22 +0800

    1.0.1

--- ZFRefresh/ZFRefresh/ViewController.swift
+++ ZFRefresh/ZFRefresh/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UITableViewController {
         self.navigationItem.title = "ZFRefresh"
         tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: ID)
         
-        // 1.创建刷新控件，添加刷新控件监听
+        // 1.创建刷新控件，添加监听
         refreshControl = ZFRefreshControl(navBar: (navigationController?.navigationBar)!)
         refreshControl?.addTarget(self, action: "loadData", forControlEvents: .ValueChanged)
     }
@@ -23,19 +23,16 @@ class ViewController: UITableViewController {
     @objc private func loadData() {
     
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(1.5 * Double(NSEC_PER_SEC))), dispatch_get_main_queue()) { () -> Void in
-            
             // 2.网络请求成功或者失败后，结束刷新
             self.refreshControl?.zf_endRefreshing(10)
         }
     }
 }
 
-// MARK:- UITableView数据源
 extension ViewController {
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 100
     }
-    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(ID, forIndexPath: indexPath)
         cell.textLabel?.text = "测试数据\(indexPath.row)"
